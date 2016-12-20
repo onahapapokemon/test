40e02443ac630f4884edaa6da9ed074966aa2f25
onahapapokemon <onahapapokemon@mail.ru>
2016-12-20 12:59:52 +0500

    Добавления класса Point и сокрытие всего что косается точки, в нем

---
+++ Snake/Snake/Point.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Point
+    {
+        /// <summary>
+        /// int x - координата точки по х
+        /// int y - координата точки на у
+        /// char sym - выводимый символ
+        /// 
+        /// мутод Draw выводит на экран символ в заданных координатах х и у
+        /// </summary>
+
+        public int x;
+        public int y;
+        public char sym;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
