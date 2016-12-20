0cd381ecc8e121b9d6d6dea6c21e56aee3f55a3a
onahapapokemon <onahapapokemon@mail.ru>
2016-12-20 16:19:02 +0500

    Добавления setBufferSize для установки размера окна

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,17 +10,20 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            //размер окна
+            Console.SetBufferSize(120, 30);
+
             //отрисовка линий
             HLine hLineUp = new HLine(0, 118, 0, '*');
             hLineUp.Drow();
 
-            HLine hLineDown = new HLine(0, 118, 28, '*');
+            HLine hLineDown = new HLine(0, 118, 29, '*');
             hLineDown.Drow();
 
-            VLine vLineLeft = new VLine(0, 28, 0, '*');
+            VLine vLineLeft = new VLine(0, 29, 0, '*');
             vLineLeft.Drow();
 
-            VLine vLineRight = new VLine(0, 28, 118, '*');
+            VLine vLineRight = new VLine(0, 29, 118, '*');
             vLineRight.Drow();
 
             Console.ReadLine();
